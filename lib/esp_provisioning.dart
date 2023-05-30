c62409fc163a2d820e5a167133f8cfc7771a919c
Tony (Thang Ngo) <tngotran@gmail.com>
2023-05-30 22:22:36 +1000

    Update esp_provisioning.dart

--- lib/esp_provisioning.dart
+++ lib/esp_provisioning.dart
@@ -2,5 +2,5 @@ export 'src/esp_prov.dart';
 export 'src/security.dart';
 export 'src/security1.dart';
 export 'src/transport.dart';
-export 'src/transport.dart';
+export 'src/TransportBLE.dart';
 export 'src/connection_models.dart';
